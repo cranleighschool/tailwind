8e42d432f497e9e27fa54438e6d4679a1ee63810
Fred Bradley <hello@fredbradley.co.uk>
2025-08-19 15:04:14 +0100

    separate colors out into separate object, so that Laravel can grab it later

--- tailwind-preset.js
+++ tailwind-preset.js
@@ -14,6 +14,8 @@ const houses = [
   "school-house",
 ];
 
+import { colors } from "@cranleighschool/tailwind/colors";
+
 const getSafelist = () =>
   houses.flatMap((color) => [
     `text-${color}`,
@@ -32,33 +34,7 @@ module.exports = {
 
   theme: {
     extend: {
-      colors: {
-        cranleigh: {
-          DEFAULT: "#021E42",
-          gold: "#B0AA7E",
-          wey: "#7D9CC0",
-          clay: "#C09C83",
-          "heathland-green": "#7F9C90",
-          "chalk-lane": "#D0D0CE",
-          "ivory-hall": "#D6D2C4",
-          "boxwood-mist": "#BAC5B9",
-        },
-
-        cubitt: { DEFAULT: "#004C45" },
-        east: { DEFAULT: "#5C88DA" },
-        south: { DEFAULT: "#279989" },
-        west: { DEFAULT: "#C16784" },
-        loveday: { DEFAULT: "#B7312C" },
-        north: { DEFAULT: "#76232F" },
-        rhodes: { DEFAULT: "#D57800" },
-        martlet: { DEFAULT: "#653379" },
-        clubs: { DEFAULT: "#228848" },
-        diamonds: { DEFAULT: "#F6BE00" },
-        hearts: { DEFAULT: "#BA0C2F" },
-        spades: { DEFAULT: "#0086D6" },
-        "school-house": { DEFAULT: "#003087" },
-      },
-
+      colors: colors,
       fontFamily: {
         body: ["proxima-nova", "sans-serif"],
         heading: ["anth", "sans-serif"],
